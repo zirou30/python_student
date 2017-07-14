a370201009dc383d3a34effbaadc8c60eebc0cbe
Zirou <z3rou@hotmail.com>
2017-07-14 18:09:07 -0300

    Update ex36.py

--- ex36.py
+++ ex36.py
@@ -7,5 +7,5 @@
 lista = ['renata', 'rafaela', 'gabriela']
 for x in lista:
     print('a banda slayer é muito boa:' + x.capitalize())
-    print('espero que logo eles façam um show no brasil:'.title()+ x.capitalize())
+    print('espero que logo eles façam um show no brasil:'.title()+ x.capitalize() + '\n')
     print('e assim esperamos'.capitalize())
